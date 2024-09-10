963a0d2e7b2aec54e5d5428f94c4070cef642e75
Angel Sosa <gabrielo.09.angel@gmail.com>
2024-09-10 03:17:25 -0500

    adding screening url deployed

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,2 @@
 export const vendorsUrl: string = 'http://localhost:19893/api/Vendors'
-export const screeningUrl: string = 'http://127.0.0.1:5000'
+export const screeningUrl: string = 'http://18.232.129.23'
